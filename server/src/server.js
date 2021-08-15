8ee70ce6f0e64a298c5ec0103d6cafa5f7c620ee
Pedro Marques <junnior.pedro.pm@gmail.com>
2021-08-15 20:07:22 -0300

    improve code style

--- server/src/server.js
+++ server/src/server.js
@@ -4,22 +4,22 @@ const { MessageController } = require('./controllers/MessageController');
 const messageController = new MessageController();
 
 const server = http.createServer(async (request, response) => {
-    if(request.url.match(/\/messages\?username=[a-zA-Z0-9_]+/) && request.method === 'GET') {
+    if (request.url.match(/\/messages\?username=[a-zA-Z0-9_]+/) && request.method === 'GET') {
         const username = request.url.split('=')[1]
         await messageController.index(request, response, username);
-    } else if(request.url.match(/\/messages\/[a-zA-Z0-9_-]+/) && request.method === 'GET') {
+    } else if (request.url.match(/\/messages\/[a-zA-Z0-9_-]+/) && request.method === 'GET') {
         const id = request.url.split('/')[2]
         await messageController.show(request, response, id);
-    } else if(request.url.match(/\/messages\/[a-zA-Z0-9_-]+/) && request.method === 'DELETE') {
+    } else if (request.url.match(/\/messages\/[a-zA-Z0-9_-]+/) && request.method === 'DELETE') {
         const id = request.url.split('/')[2]
         await messageController.destroy(request, response, id);
-    } else if(request.url.match(/\/messages\/[a-zA-Z0-9_-]+/) && request.method === 'PUT') {
+    } else if (request.url.match(/\/messages\/[a-zA-Z0-9_-]+/) && request.method === 'PUT') {
         const id = request.url.split('/')[2]
         await messageController.update(request, response, id);
-    } else if(request.url === '/message' && request.method === 'GET') {
-        response.writeHead(200, {'Content-Type': 'application/json'});
+    } else if (request.url === '/message' && request.method === 'GET') {
+        response.writeHead(200, { 'Content-Type': 'application/json' });
         response.end(JSON.stringify({ ok: true }));
-    } else if(request.url === '/message' && request.method === 'POST') {
+    } else if (request.url === '/message' && request.method === 'POST') {
         await messageController.create(request, response);
     }
 });
